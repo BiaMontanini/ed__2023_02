fe67c36353a31569c1c40d52e4ca672f85444af4
Beatriz R. Montanini <montanini249@gmail.com>
2023-08-25 20:55:49 -0300

    exercicio 1.1, 1.2, 1.3

---
+++ exercicio-dois/src/exercicio1.ts
@@ -0,0 +1,25 @@
+var a: number = 10;
+var b: string = "Fatec";
+var c: boolean = true;
+var f;
+f=10;
+f;
+f="a";
+a="a";
+
+var json = {
+  a: "a",
+  b: 1,
+  C: "c",
+};
+
+var array: number[] = [1, 2, 3, 4, 5, 6];
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+console.log(json, typeof json);
+console.log(array, typeof array);
+
+
+/*RESPOSTA DA QUESTÃO P.1.2: AO PASSAR O MOUSE APARECE A TIPAGEM QUE FOI ADICIONADA A VARIAVEL, AO FAZER ESTA MARCAÇÃO NÃO É POSSIVEL ALTERA-LA DEPOIS, SE A TIPAGEM FOR "ANY" OU SEJA NENHUMA, A VARIAVEL VAI ACEITAR QUALQUER TIPO DE DADO, SE FOR NUMBER APENAS NUMERO E STRING APENAS STRING*/
---
+++ exercicio-dois/src/exercicio2.ts
@@ -0,0 +1,12 @@
+const Maior = (a: number, b: number) => {
+  if (a > b) {
+    return a;
+  } else {
+    if (a==b){return "É igual"}
+    else{
+    return b;
+}
+  }
+};
+
+console.log(Maior(2, 2));
